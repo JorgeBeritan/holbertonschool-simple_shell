e6d0d5ebcec15e309ce93015e9d76a0dd5cc0dcb
nachoneca <8666@holbertonstudents.com>
2024-04-25 16:50:34 +0000

    fixing

--- function.c
+++ function.c
@@ -90,7 +90,7 @@ int command_read(char *line, size_t nread)
 	if (strcmp(line, "exit") == 0)
 	{
 		free(line);
-		return (0);
+		exit(0);
 	}
 	/**analizamos el caso de env y su solucion*/
 	if (strcmp(line, "env") == 0)
